12eaa83fc0139af6ce86ab9705c41dcc50591e8d
shivanshaggarwal <bhupeshamazon2072@gmail.com>
2025-09-05 19:05:15 +0530

    changes

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 console.log('Base url-', process.env.REACT_APP_API_BASE);
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE||'https://splitit-backend-i09x.onrender.com',
-    withCredentials: false
+    baseURL: process.env.REACT_APP_API_BASE||'https://splitit-backend-i09x.onrender.com/api',
+    withCredentials: true
 })
 api.interceptors.request.use((config)=>{
     const token = localStorage.getItem('token');
